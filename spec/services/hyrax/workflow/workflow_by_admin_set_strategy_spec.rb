1672a0f193a8fec9eb4a6e260c60f009f80f8a2d
Aaron Collier <acollier@calstate.edu>
2017-01-18 08:25:41 -0800

    Fixing tests.

--- spec/services/hyrax/workflow/workflow_by_admin_set_strategy_spec.rb
+++ spec/services/hyrax/workflow/workflow_by_admin_set_strategy_spec.rb
@@ -4,14 +4,14 @@ describe Hyrax::Workflow::WorkflowByAdminSetStrategy, :no_clean do
 
     describe '#workflow_id' do
       subject { workflow_strategy.workflow_id }
-      it { is_expected.to eq '1' }
+      it { is_expected.to eq 1 }
     end
   end
 
   context "when using a non-default workflow strategy" do
     let!(:admin_set) { AdminSet.create(title: ["test"]) }
     let!(:permission_template) { Hyrax::PermissionTemplate.create(workflow_id: workflow_id, admin_set_id: admin_set.id) }
-    let(:workflow_name) { 'work' }
+    let(:workflow_id) { 1 }
     let(:workflow_strategy) { described_class.new(nil, admin_set_id: admin_set.id) }
 
     describe '#workflow_id' do
